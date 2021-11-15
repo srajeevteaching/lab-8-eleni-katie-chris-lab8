7e5fb648525ec3a2ef17b8bc06318a6e2ac1db90
baseb <cmplowman@loyola.edu>
2021-11-14 19:14:36 -0500

    Push-2

--- Lab8.py
+++ Lab8.py
@@ -4,39 +4,34 @@
 #Lab8
 
 #take in list
+#turn it into a string
 #randomize it
 #pop out name of student that is used
 #continue if desired
 
-
 import random
 import sys
+
 def load_name_list(fname):
     list = []
     file_name = open(fname ,"r")
     for line in file_name:
         list.append(line)
-        print(line)
     file_name.close()
     return list
 
 def pop_random_name(list1):
-    student_name = random.randrange(list1)
-    list1.pop(student_name)
-    return student_name
+    if len(list1) > 0:
+       return list1.pop(random.randrange(len(list1)))
+    else:
+        print("All out of names!")
+        sys.exit()
 
 def main():
     list1 = load_name_list("Lab8-names.txt")
-    name = pop_random_name(list1)
-    pop_random_name(list1)
-    choice = input("Do you want to continue? ")
-    choice.strip().lower()
-    if choice == "yes":
-        pop_random_name(list1)
-    elif choice == "no":
-        sys.exit()
-    else:
-        print("Invalid entry")
-
-
+    while True:
+        print(pop_random_name(list1))
+        if input("press [y] for another name ") == "y":
+            continue
+        break
 main()
